f3509255aa1f619d67c249768a06be101cb03683
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-07-23 21:25:31 +0700

    update action change

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from locaters.inventory import LocInventory
+from selenium.webdriver.common.action_chains import ActionChains
 
 class Inventory :
     def __init__(self, driver):
@@ -9,3 +10,12 @@ class Inventory :
         title = self.driver.find_element(By.XPATH,LocInventory.title_text ).text
 
         return title
+
+    def sort_atoz(self):
+        
+        selector = self.driver.find_element(By.XPATH,"//select[@class='product_sort_container']")
+        option_atoz = self.driver.find_element(By.XPATH,"//select[@class='product_sort_container']/option[text()='Name (A to Z)']")
+        
+        ActionChains.click(selector)
+        ActionChains.click(option_atoz)
+        ActionChains.perform()
