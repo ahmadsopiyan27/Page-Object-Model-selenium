88f147d947ff10383f2a37dc1bec1d34e1dbfcfc
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-07-24 21:52:42 +0700

    update

--- locaters/inventory.py
+++ locaters/inventory.py
@@ -1,2 +1,3 @@
 class LocInventory :
     title_text = '//*[@id="header_container"]/div[1]/div[2]/div'
+    
--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -6,9 +6,11 @@ class Inventory :
     def __init__(self, driver):
         self.driver = driver
 
+
     def check_title(self):
         title = self.driver.find_element(By.XPATH,LocInventory.title_text ).text
 
         return title
+    
     
     
