57d9363f5e59e4edc3d4d3155cf3925e5153063c
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-07-25 09:45:17 +0700

    pom cart

---
+++ locaters/cart.py
@@ -0,0 +1,14 @@
+
+
+class LocCart:
+    add_cart_backpack = 'add-to-cart-sauce-labs-backpack'
+    add_cart_bike_light = 'add-to-cart-sauce-labs-bike-light'
+    add_cart_tshirt = 'add-to-cart-sauce-labs-bolt-t-shirt'
+    cart_icon = 'shopping_cart_link'
+    checkout_button = 'checkout'
+    first_name_input = 'first-name'
+    last_name_input = 'last-name'
+    postal_code_input = 'postal-code'
+    continue_button = 'continue'
+    finish_button = 'finish'
+    order_status_text = 'complete-header'
